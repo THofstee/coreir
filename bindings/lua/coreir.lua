34d76a6cca3a21431686b7a0f7441c3d469ce5cc
Teguh Hofstee <teguhhofstee@gmail.com>
2017-05-08 17:43:28 -0400

    update makefile

--- bindings/lua/coreir.lua
+++ bindings/lua/coreir.lua
@@ -8,9 +8,11 @@ end
 local header_path = './'
 local lib_path = '../../lib/'
 
+io.write(package.path .. '\n')
+io.write(package.cpath .. '\n')
+
 ffi = require('ffi') -- luajit ffi
 ffi.cdef(read_file(header_path .. 'coreir-single.h'))
 coreir = ffi.load(lib_path .. 'libcoreir-c.so')
 
 test_gen = read_file('_add4Gen.json')
-io.write(test_gen)
