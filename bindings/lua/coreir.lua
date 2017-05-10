b2ecc5a4fdc742692ac272152615dadfa646c2c1
Teguh Hofstee <teguhhofstee@gmail.com>
2017-05-10 00:53:11 -0400

    update makefile to load stdlib, update lua

--- bindings/lua/coreir.lua
+++ bindings/lua/coreir.lua
@@ -1,5 +1,8 @@
 local coreir = {}
 
+local header_path = './'
+local lib_path = '../../lib/'
+
 local function read_file(file)
     local f = io.open(file, "rb")
     local content = f:read("*all")
@@ -7,13 +10,28 @@ local function read_file(file)
     return content
 end
 
-local header_path = './'
-local lib_path = '../../lib/'
+local function init()
+	  local ffi = require('ffi')
+	  ffi.cdef(read_file(header_path .. 'coreir-single.h'))
+	  return ffi.load(lib_path .. 'libcoreir-c.so')
+end
+
+local function load_lib(lib)
+	  local ffi = require('ffi')
+	  ffi.cdef("CORENamespace* CORELoadLibrary_" .. lib .. "(COREContext* c);")
+	  return ffi.load(lib_path .. 'libcoreir-' .. lib .. '.so')
+end
 
 local ffi = require('ffi') -- luajit ffi
-ffi.cdef(read_file(header_path .. 'coreir-single.h'))
-coreir_lib = ffi.load(lib_path .. 'libcoreir-c.so')
+coreir_lib = init()
+coreir_stdlib = load_lib('stdlib')
 
-local test_gen = read_file('_add4Gen.json')
+local test_ctx = coreir_lib.CORENewContext()
+local module_str = "_add4Gen.json"
+local module_cstr = ffi.new("char[?]", #module_str)
+ffi.copy(module_cstr, module_str)
+
+local stdlib = coreir_stdlib.CORELoadLibrary_stdlib(test_ctx)
+local test_gen = coreir_lib.CORELoadModule(test_ctx, module_cstr, err)
 
 return coreir
