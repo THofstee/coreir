7a188e82a83655df1c2db7512d2528cbcab93f38
Teguh Hofstee <teguhhofstee@gmail.com>
2017-06-01 01:01:39 -0500

    whitespace

--- src/typegen.cpp
+++ src/typegen.cpp
@@ -6,12 +6,9 @@
 using namespace std;
 
 namespace CoreIR {
-
-
-Type* TypeGen::getType(Args args) {
-  checkArgsAreParams(args,params);
-  Type* t = this->createType(ns->getContext(),args);
-  return flipped ? t->getFlipped() : t;
-}
-
+	Type* TypeGen::getType(Args args) {
+		checkArgsAreParams(args,params);
+		Type* t = this->createType(ns->getContext(),args);//HERE #2
+		return flipped ? t->getFlipped() : t;
+	}
 }
